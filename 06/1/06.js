ff5e45a337e96c1f0a59d3503af8094093913047
facucanis <facucanis@gmail.com>
2021-09-15 19:55:23 -0300

    Desafío: Incorporar Arrays

---
+++ 06/1/06.js
@@ -0,0 +1,10 @@
+let array = [];
+
+function insertarEnArray(elemento) {
+  array.push(elemento);
+  return array;
+}
+
+console.log(insertarEnArray("Primer Elemento"));
+console.log(insertarEnArray("Segundo Elemento"));
+console.log(insertarEnArray("Tercer Elemento"));
